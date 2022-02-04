f55ca842eed9eeef2ea6759b466b40f8476db9a6
Miles Cochran-Branson <mcochranbranson@gmail.com>
2022-02-04 08:32:13 -0600

    plot edits

--- examples/example.jl
+++ examples/example.jl
@@ -7,13 +7,13 @@ data = read_Txt("examples/exampleData.Txt")
 x0, y0 = get_xy_data(data, 100, f)
 
 # Plot raw data
-plot(x0, y0, label = "data", xlabel = "Energy [keV]", ylabel = "Counts", legend=:topleft)
+plot(x0, y0, label = "data", xlabel = "Energy [keV]", ylabel = "Counts", legend=:topleft, dpi=400, size=(350, 350))
 
 # extract smoothed curve and peak data
 peaks, ySmooth = peak_parameters(x0, y0, 25, 1000.0, 0.5)
 
 #plot this new data
-plt = plot(x0, ySmooth, label = "data", xlabel = "Energy [keV]", ylabel = "Counts", legend=:topleft)
+plt = plot(x0, ySmooth, label = "data", xlabel = "Energy [keV]", ylabel = "Counts", legend=:topleft, dpi=400, size=(350, 350))
 for i in 1:length(peaks)
     scatter!(x0[peaks[i]], ySmooth[peaks[i]], label = "Peak Data $i")
 end
