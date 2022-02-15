992e3e2b5ae195f62e45941505e9b455e18b4ddd
Miles Cochran-Branson <mcochranbranson@gmail.com>
2022-02-15 17:48:06 -0600

    add comments to src

--- src/LUComptonScatter.jl
+++ src/LUComptonScatter.jl
@@ -1,13 +1,14 @@
 module LUComptonScatter
 
+#dependencies 
 import LinearAlgebra
 import DataFrames
 import Findpeaks
 import Smoothers
 import Interpolations
 
+#functions for peak finding and analysis
 include("analysisFuncs.jl")
-
 export expected_energy
 export read_Txt
 export get_xy_data
@@ -19,8 +20,8 @@ export get_peak_value
 export gauss
 export fit_to_gauss
 
+#functions for data fitting
 include("chiSquredFitting.jl")
-
 export χ²
 export compton
 export scan
